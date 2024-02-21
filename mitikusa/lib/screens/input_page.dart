be03b432a5cda1c542098cf542e5be1608d4f254
Kawasaki Yuichi <yuyux0910@gmail.com>
2024-02-22 00:05:38 +0900

    [add]検索情報入力ページに出発地と目的地の入力欄を作成

--- mitikusa/lib/screens/input_page.dart
+++ mitikusa/lib/screens/input_page.dart
@@ -9,12 +9,68 @@ class MyInputPage extends StatefulWidget {
 }
 
 class MyInputPageState extends State<MyInputPage> {
+  // テキストフィールドコントローラーの宣言
+  // 出発地
+  final TextEditingController _inputDepartController = TextEditingController();
+
+  @override
+  void dispose() {
+    _inputDepartController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text('Input Page ${widget.destination}'),
+          title: const Text('検索')
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+            child: Row(
+              children: <Widget>[
+                const Text('出発地'),
+                const Spacer(),
+                Flexible(
+                    child: SizedBox(
+                      child: TextField(
+                        controller: TextEditingController(text: '現在地'),
+                      ),
+                    )
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+            child: Row(
+              children: <Widget>[
+                const Text('目的地'),
+                const Spacer(),
+                Flexible(
+                    child: SizedBox(
+                      child: TextField(
+                        controller: TextEditingController(text: widget.destination),
+                      ),
+                    )
+                ),
+              ],
+            ),
+          ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40), child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('決定'),
+                ),
+              ],
+            )
+          ),
+        ],
       ),
     );
   }
